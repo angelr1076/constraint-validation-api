c1cd2a363165b6f89bb6824d106c9ec22821f3d6
angelr1076 <angelr1076@gmail.com>
2023-01-11 15:25:10 -0500

    Updated css and removed unneccessary elements

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const form = document.querySelector('form');
-const uname = document.getElementById('uname');
 const email = document.querySelector('#email');
-const phone = document.querySelector('#phone');
-const comment = document.querySelector('#comment');
 const errorMsgSpan = document.querySelector('.error');
 
 // event listeners for each element
@@ -14,19 +11,8 @@ form.addEventListener('submit', e => {
   }
 });
 
-uname.addEventListener('input', e => {
-  if (!uname.validity.patternMismatch) {
-    // reset error message view
-    errorMsgSpan.textContent = '';
-    errorMsgSpan.className = 'error';
-  } else {
-    console.log(uname.validationMessage);
-    showError();
-  }
-});
-
 email.addEventListener('input', e => {
-  if (!email.validity.patternMismatch) {
+  if (!email.validity.tooShort && !email.validity.patternMismatch) {
     // reset error message view
     errorMsgSpan.textContent = '';
     errorMsgSpan.className = 'error';
@@ -36,30 +22,14 @@ email.addEventListener('input', e => {
   }
 });
 
-phone.addEventListener('input', e => {
-  if (!phone.validity.patternMismatch) {
-    // reset error message view
-    errorMsgSpan.textContent = '';
-    errorMsgSpan.className = 'error';
-  } else {
-    console.log(phone.validationMessage);
-    showError();
-  }
-});
-
 function showError() {
-  if (uname.validity.patternMismatch) {
+  if (email.validity.patternMismatch) {
     errorToggle();
     errorMsgSpan.textContent =
-      "Must be only a single letter from 'a' to 'z', a dot, then 3 or more letters/numbers(lowercase).";
-  } else if (email.validity.patternMismatch) {
-    errorToggle();
+      'All email addresses must contain an @ symbol... Example: BigRed25@bigcorp.eu';
+  } else if (email.validity.tooShort) {
     errorMsgSpan.textContent =
-      'All email addresses must consist of one or more letters (lower or upper case) or numbers, followed by "@bigcorp.eu". Example: BigRed25@bigcorp.eu';
-  } else if (phone.validity.patternMismatch) {
-    errorToggle();
-    errorMsgSpan.textContent =
-      'Phone entries must be either 10 digits in a row or a pattern of three digits, three digits, then four digits, separated by either spaces, dashes, or dots';
+      'Your email address must be at least 10 characters long';
   }
 }
 
