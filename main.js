2fd86e88728402ce832134daad6c932426e19e14
angelr1076 <angelr1076@gmail.com>
2023-01-11 15:42:25 -0500

    Updated form eventListener to show submit message and clear email field

--- main.js
+++ main.js
@@ -8,28 +8,33 @@ form.addEventListener('submit', e => {
   if (!form.checkValidity()) {
     console.log(e);
     showError();
+  } else {
+    errorToggle();
+    errorMsgSpan.textContent = 'Form submitted.';
+    email.value = '';
   }
 });
 
 email.addEventListener('input', e => {
-  if (!email.validity.tooShort && !email.validity.patternMismatch) {
+  if (email.validity.tooShort || email.validity.patternMismatch) {
+    console.log(email.validationMessage);
+    showError();
+  } else {
     // reset error message view
     errorMsgSpan.textContent = '';
     errorMsgSpan.className = 'error';
-  } else {
-    console.log(email.validationMessage);
-    showError();
   }
 });
 
 function showError() {
-  if (email.validity.patternMismatch) {
+  if (email.validity.tooShort) {
+    errorToggle();
+    errorMsgSpan.textContent =
+      'Your email address must be at least 10 characters long';
+  } else if (email.validity.patternMismatch) {
     errorToggle();
     errorMsgSpan.textContent =
       'All email addresses must contain an @ symbol... Example: BigRed25@bigcorp.eu';
-  } else if (email.validity.tooShort) {
-    errorMsgSpan.textContent =
-      'Your email address must be at least 10 characters long';
   }
 }
 
